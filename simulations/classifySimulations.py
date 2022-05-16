59d32026dceb9795bcf68d40403c16c52bec1ed4
Gordon Julian Köhn <gordon@koehn.net>
2022-05-16 13:57:01 +0200

    Remove excitatory/inhibitory distinction + fix frequency mistake

--- simulations/classifySimulations.py
+++ simulations/classifySimulations.py
@@ -9,9 +9,10 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import math
+from elephant.statistics import cv, isi
 
 
-def getFreq(path):
+def getFreq(result):
     """
     Get mean firing frequencies of a given file behin the given path
     
@@ -23,43 +24,21 @@ def getFreq(path):
     
     Returns
     -------
-    rates_Hz_ex : np.array
-        frequencies in Hz of spikes per excitatory neuron
-    rates_Hz_in : np.array
-        frequencies in Hz of spikes per inhibitory neuron
+    freqs : np.array
+        frequencies in Hz of spikes per excitatory and inhibitory neuron
+    
     
     """    
+
+    ##### Unpack data ###########
+    # unpack spikes
+    ids=np.append( result['in_idx'],  result['ex_idx'])  
     
-    root_dir = 'simData'
-
-    curr_dir = root_dir + '/' 
-    curr_dir += 'N_' +str(params['N']) + '/'  
-    curr_dir +=  '_'.join(['p', str(params['prob_Pee']),str(params['prob_Pei']), str(params['prob_Pii']), str(params['prob_Pie'])])
-
-
-    result = np.load(curr_dir + '/' + save_name, allow_pickle=True)
-    result = result.tolist() # convert from array back to dictionary
-
-    index, counts = np.unique( result['ex_idx'], return_counts=True)
-    rates_Hz_ex = np.asarray(counts/1000)
-
-
-    index, counts = np.unique(result['in_idx'], return_counts=True)
-    rates_Hz_in = np.asarray(counts/1000)
+    # get freq
+    index, counts = np.unique(ids, return_counts=True)
+    freqs = np.asarray(counts/result['sim_time']) #in Hz
     
-    
-    #fig7, ax7 = plt.subplots()
-    #green_diamond = dict(markerfacecolor='g', marker='D')
-    #data = [rates_Hz_ex, rates_Hz_in]
-    #ax7.set_title('Mean Firing Frequency')
-    #bp = ax7.boxplot(data, flierprops=green_diamond)
-    #ax7.set_xticklabels(("excitatory", "inhibitory"), size=10)
-    #ax7.set_xlabel('Neuron Type')
-    #ax7.set_ylabel('Hz')
-    #ax7.grid()
-    #fig7.show()
-    
-    return rates_Hz_ex, rates_Hz_in
+    return freqs
 
 
 
@@ -79,7 +58,6 @@ def getPath(params):
         path to .npy simulation file
     
     """   
-    
     root_dir = 'simData'
 
     curr_dir = root_dir + '/' 
@@ -105,7 +83,6 @@ def getFilename(params):
         name of simulation file
     
     """   
-    
     save_name = '_'.join([str(params['N']), str(params['a']),str(params['b']), str(params['sim_time']) ]) + '_'
     save_name += '_'.join([str(params['ge']), str(params['gi']),  str(params['prob_Pee']),str(params['prob_Pei']), str(params['prob_Pii']), str(params['prob_Pie']) ])
     save_name += ".npy"
@@ -113,9 +90,39 @@ def getFilename(params):
     return save_name
 
 
-
-if __name__ == '__main__':
+def getResult(curr_dir, save_name):
+    """
+    Get simulation results given path and filename.
     
+    Parameters
+    ----------
+    curr_dir : str
+       path to file from current directory
+       
+    save_name : str
+        name file was saved as
+        
+        
+    Returns
+    -------
+    result : dict
+        see wp2_adex_model_script.py
+    
+    """  
+    root_dir = 'simData'
+
+    curr_dir = root_dir + '/' 
+    curr_dir += 'N_' +str(params['N']) + '/'  
+    curr_dir +=  '_'.join(['p', str(params['prob_Pee']),str(params['prob_Pei']), str(params['prob_Pii']), str(params['prob_Pie'])])
+
+    result = np.load(curr_dir + '/' + save_name, allow_pickle=True)
+    result = result.tolist() # convert from array back to dictionary
+    return result
+    
+
+###############################################################################
+###############################################################################
+if __name__ == '__main__':
     
     ######################################################
     ########### specify simulation parameters ############
@@ -146,37 +153,56 @@ if __name__ == '__main__':
     
     
     ############## loop through all files ################
-    ges = []
+    ## define output lists
+    # conductance values
+    ges = [] 
     gis = []
-    m_freqs_ex = []
-    m_freqs_in = []
+    #mean firing frequencies
+    m_freqs = []
+    # coefficeit of varriation
+    cvs = []
+    # mean pairwise correlation
+    #TODO: mean pairwise correlation
     
+    # loop through all conditions and analyze files
     for gi in np.arange(gi_min,gi_max+step,step):
         for ge in np.arange(gi_min,gi_max+step,step):
+            # define parameters
             params['ge']=float(ge)
             params['gi']=float(gi)
-    
+            # get file
             save_name = getFilename(params)
     
             curr_dir = getPath(params)
-        
-            rates_Hz_ex, rates_Hz_in = getFreq(curr_dir)
-    
-            m_rates_Hz_ex = np.mean(rates_Hz_ex)
-            m_rates_Hz_in = np.mean(rates_Hz_in)
             
+            # get data
+            result = getResult(curr_dir, save_name)
+            
+            ## analyze
+            # get mean firing frequencies
+            freqs = getFreq(result)
+    
+            m_freq = np.mean(freqs)
+            
+            # get coefficient of variation
+            #result['ex_time']= timeE
+            #result['in_time']= timeI
+            
+            #cvs.append(cv(isi(trains)))
+            
+            #save data
             ges.append(ge)
             gis.append(gi)
-            m_freqs_ex.append(m_rates_Hz_ex)
-            m_freqs_in.append(m_rates_Hz_in)
+            m_freqs.append(m_freq)
+       
             
     # create output dictonary
     condFreqSpace =  dict()
     condFreqSpace['ge'] = ges
     condFreqSpace['gi'] = gis
     # save frequency values but replace NAs with 0
-    condFreqSpace['m_freq_ex'] = [0 if x != x else x for x in m_freqs_ex] 
-    condFreqSpace['m_freq_in'] = [0 if x != x else x for x in m_freqs_in] 
+    condFreqSpace['m_freqs'] = [0 if x != x else x for x in m_freqs] 
+
     
     #######################################################
     
@@ -194,25 +220,8 @@ if __name__ == '__main__':
     ax = plt.axes(projection ="3d")
  
     # Creating plot
-    ax.scatter3D(condFreqSpace['ge'],condFreqSpace['gi'] , condFreqSpace['m_freq_ex'], color = "green")
-    plt.title("conductance-frequency space for excitatory")
-    
-    ax.set_xlabel('ge [nS]')
-    ax.set_ylabel('gi [nS]')
-    ax.set_zlabel('mean freq. [Hz]')
- 
-    # show plot
-    plt.show()
-    
-    #### make inhibitory 
-    
-    # Creating figure
-    fig = plt.figure(figsize = (10, 7))
-    ax = plt.axes(projection ="3d")
- 
-    # Creating plot
-    ax.scatter3D(condFreqSpace['ge'],condFreqSpace['gi'] , condFreqSpace['m_freq_in'], color = "green")
-    plt.title("conductance-frequency space for inhibitory")
+    ax.scatter3D(condFreqSpace['ge'],condFreqSpace['gi'] , condFreqSpace['m_freqs'], color = "green")
+    plt.title("conductance-frequency space")
     
     ax.set_xlabel('ge [nS]')
     ax.set_ylabel('gi [nS]')
