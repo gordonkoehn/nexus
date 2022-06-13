9701a68d8cd13226eaba27e17cd0a05fd36c155b
Gordon Koehn <gkoehn@d.ethz.ch@bs-hierlemann07.ethz.ch>
2022-06-13 11:52:40 +0200

    add runAll script to execute all bash at once

---
+++ simulations/gridSearch/adapParrShGrid/runAll.sh
@@ -0,0 +1,11 @@
+nohup ./replicateOneCondition_b1.sh > b1.out & 
+nohup ./replicateOneCondition_b2.sh > b2.out & 
+nohup ./replicateOneCondition_b3.sh > b3.out & 
+nohup ./replicateOneCondition_b4.sh > b4.out & 
+nohup ./replicateOneCondition_b5.sh > b5.out & 
+nohup ./replicateOneCondition_b6.sh > b7.out & 
+nohup ./replicateOneCondition_b7.sh > b9.out & 
+nohup ./replicateOneCondition_b8.sh > b8.out & 
+nohup ./replicateOneCondition_b9.sh > b9.out & 
+nohup ./replicateOneCondition_b10.sh > b10.out & 
+nohup ./replicateOneCondition_b11.sh > b11.out & 
