cef421f579f5a37ddbd9344856aa2854cfead551
Gordon Julian Köhn <gordon@koehn.net>
2022-06-13 11:57:01 +0200

    Add conductance search with replica

---
+++ simulations/gridSearch/condParrShGrid/runAll.sh
@@ -0,0 +1,8 @@
+nohup ./replicateOneCondition_gi_1.sh > gi1.out & 
+nohup ./replicateOneCondition_gi_2.sh > gi2.out & 
+nohup ./replicateOneCondition_gi_3.sh > gi3.out & 
+nohup ./replicateOneCondition_gi_4.sh > gi4.out & 
+nohup ./replicateOneCondition_gi_5.sh > gi5.out & 
+nohup ./replicateOneCondition_gi_6.sh > gi6.out & 
+nohup ./replicateOneCondition_gi_7.sh > gi7.out & 
+nohup ./replicateOneCondition_gi_8.sh > gi8.out & 
